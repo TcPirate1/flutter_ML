1e52c1dd1111637d3968b46a6358943e946d79cd
TC_Linux <88120195+TcPirate1@users.noreply.github.com>
2025-02-20 16:02:03 +1300

    Inserted training data as assets in project. Pytesseract already has it probably which is why it successfully gets data.

--- flutter_application_cards/lib/preprocess_img.dart
+++ flutter_application_cards/lib/preprocess_img.dart
@@ -1,9 +1,13 @@
+import 'dart:io';
+
 import 'package:opencv_core/opencv.dart';
 import 'package:flutter_tesseract_ocr/flutter_tesseract_ocr.dart';
 
-Future<void> isImgBlurry(Mat img, {double threshold = 100.0}) async {
+Future<void> isImgBlurry(File img, {double threshold = 100.0}) async {
+  final test = imread(img.path);
+
   // Convert to grayscale
-  final Mat grayImg = cvtColor(img, COLOR_BGR2GRAY);
+  final Mat grayImg = cvtColor(test, COLOR_BGR2GRAY);
 
   // Use laplacian method
   Mat lapImg = laplacian(grayImg, MatType.CV_64F);
@@ -16,10 +20,10 @@ Future<void> isImgBlurry(Mat img, {double threshold = 100.0}) async {
     print("Not blurry");
   }
   
-  Mat rgbColor = cvtColor(lapImg, COLOR_BGR2RGB);
+  Mat rgbColor = cvtColor(test, COLOR_BGR2RGB);
   // CvException Unsupported depth of input image:> 'VDepth::contains(depth)'> where> 'depth' is 6 (CV_64F)
 
-  String osdData = await FlutterTesseractOcr.extractText(img.toString());
+  String osdData = await FlutterTesseractOcr.extractText(img.path, args: {"psm": "0"});
   // _AssertionError ('package:flutter_tesseract_ocr/android_ios.dart': Failed assertion: line 24 pos 12: 'await File(imagePath).exists()': true)
   
   print(osdData);
